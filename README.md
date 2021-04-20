c7982266e4b57026a1cad1fbc455d8f36d6d3287
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-04-19 21:51:21 -0300

    atividade02 criando class

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # arquitetura_AC04
-Escolha uma das atividades para entregar para AC04.
+
+Escolha uma das atividades para entregar para
+AC04 e realize a medição do tempo de execução.
+
+1) Escreva uma versão produtor consumidor
+multiprocessos.
+
+2) Escreva uma versão multithreads para
+calcular a primalidade de vários números. Após
+isso, compare o tempo de execução da sua
+solução com a solução multiprocessos
+apresentada na sala de aula.
+
+3) Pesquise sobre middleware de comunicação
+orientada a mensageria, também conhecido
+como Broker. Descreva o funcionamento do
+broker RabbitMQ.
+
+4) Criar uma aplicação que publica uma mensagem no
+broker RabbitMQ e outra que consome a mensagem.
+
+    a - Acessar https://www.cloudamqp.com/
+
+    b - criar sua fila;
+
+    c - Construir dois scripts python para enviar e receber
+mensagens da fila.
+
+Exemplo em
+https://www.rabbitmq.com/tutorials/tutorial-three-pyt
+hon.html
--- atividade02_mult_tread_primalidade.py
+++ atividade02_mult_tread_primalidade.py
@@ -9,26 +9,28 @@ from unittest import TestCase, main
 
 
 # Versão Primalidade Professor
-def eh_primo(n):
-    for i in range(3, int(n**0.5+1), 2):
-            if n % i == 0:
-                print(n, ' nao eh primo')
+class Primo():
+
+    def eh_primo(n):
+        for i in range(3, int(n**0.5+1), 2):
+                if n % i == 0:
+                    print(n, ' nao eh primo')
+                    return False
+        print(n, ' eh primo')
+        return True
+
+
+    # Minha Versão Primalidade
+    def ehPrimo_minha_versao(num):
+        for divisor in range(2, num):
+            if num % divisor == 0:
+                print(num, ' não eh primo')
                 return False
-    print(n, ' eh primo')
-    return True
-
-
-# Minha Versão Primalidade
-def ehPrimo_minha_versao(num):
-    for divisor in range(2, num):
-        if num % divisor == 0:
-            print(num, ' não eh primo')
-            return False
-    if num < 2:
-            print(num, 'não eh primo')
-            return False
-    print(num, ' eh primo')
-    return True
+        if num < 2:
+                print(num, 'não eh primo')
+                return False
+        print(num, ' eh primo')
+        return True
 
 
 class MyTestCase(TestCase):
@@ -42,12 +44,14 @@ if __name__ == '__main__':
 
     numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13] 
     pool = Pool()
+    primo_versao_um = Primo
+    primo_versao_dois = Primo
     
     print("Execução 01")
     print()
     time.sleep(3)
     inicio_A = time.time()
-    pool.map(eh_primo, numeros)
+    pool.map(primo_versao_um.eh_primo, numeros)
     fim_A = time.time()
     tempo_execucao_A = fim_A - inicio_A
     tempo_execucao_A = tempo_execucao_A * 1000
@@ -58,7 +62,7 @@ if __name__ == '__main__':
     print()
     time.sleep(3)
     inicio_B = time.time()
-    pool.map(ehPrimo_minha_versao, numeros)
+    pool.map(primo_versao_dois.ehPrimo_minha_versao, numeros)
     fim_B = time.time()
     tempo_execucao_B = fim_B - inicio_B
     tempo_execucao_B = tempo_execucao_B * 1000
